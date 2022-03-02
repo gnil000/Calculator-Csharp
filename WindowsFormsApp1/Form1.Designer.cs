92ede80cc075ed58f965d3d51157a229b8ae404a
GNIL <tony.vinz.one@gmail.com>
2022-03-02 12:02:14 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿using System;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.TextBox();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonMulti = new System.Windows.Forms.Button();
+            this.buttonDivided = new System.Windows.Forms.Button();
+            this.answer = new System.Windows.Forms.TextBox();
+            this.buttonEquals = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(22, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 109);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(146, 230);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 109);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(270, 230);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 109);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(22, 345);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 109);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            this.button5.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Location = new System.Drawing.Point(146, 345);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 109);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Black;
+            this.button6.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button6.Location = new System.Drawing.Point(270, 345);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 109);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Black;
+            this.button7.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button7.Location = new System.Drawing.Point(22, 460);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(118, 109);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Black;
+            this.button8.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button8.Location = new System.Drawing.Point(146, 460);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(118, 109);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Black;
+            this.button9.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button9.Location = new System.Drawing.Point(270, 460);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(118, 109);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button0
+            // 
+            this.button0.BackColor = System.Drawing.Color.Black;
+            this.button0.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button0.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button0.Location = new System.Drawing.Point(146, 575);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(118, 109);
+            this.button0.TabIndex = 9;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = false;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.input.Location = new System.Drawing.Point(22, 163);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(366, 61);
+            this.input.TabIndex = 10;
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.BackColor = System.Drawing.Color.Black;
+            this.buttonPlus.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPlus.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonPlus.Location = new System.Drawing.Point(427, 169);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(118, 109);
+            this.buttonPlus.TabIndex = 11;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = false;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.BackColor = System.Drawing.Color.Black;
+            this.buttonMinus.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMinus.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonMinus.Location = new System.Drawing.Point(427, 284);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(118, 109);
+            this.buttonMinus.TabIndex = 12;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = false;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonMulti
+            // 
+            this.buttonMulti.BackColor = System.Drawing.Color.Black;
+            this.buttonMulti.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMulti.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonMulti.Location = new System.Drawing.Point(427, 399);
+            this.buttonMulti.Name = "buttonMulti";
+            this.buttonMulti.Size = new System.Drawing.Size(118, 109);
+            this.buttonMulti.TabIndex = 13;
+            this.buttonMulti.Text = "*";
+            this.buttonMulti.UseVisualStyleBackColor = false;
+            this.buttonMulti.Click += new System.EventHandler(this.buttonMulti_Click);
+            // 
+            // buttonDivided
+            // 
+            this.buttonDivided.BackColor = System.Drawing.Color.Black;
+            this.buttonDivided.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDivided.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonDivided.Location = new System.Drawing.Point(427, 514);
+            this.buttonDivided.Name = "buttonDivided";
+            this.buttonDivided.Size = new System.Drawing.Size(118, 109);
+            this.buttonDivided.TabIndex = 14;
+            this.buttonDivided.Text = "/";
+            this.buttonDivided.UseVisualStyleBackColor = false;
+            this.buttonDivided.Click += new System.EventHandler(this.buttonDivided_Click);
+            // 
+            // answer
+            // 
+            this.answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.answer.Location = new System.Drawing.Point(22, 42);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(366, 61);
+            this.answer.TabIndex = 15;
+            this.answer.TextChanged += new System.EventHandler(this.Answer_TextChanged);
+            // 
+            // buttonEquals
+            // 
+            this.buttonEquals.BackColor = System.Drawing.Color.Black;
+            this.buttonEquals.Font = new System.Drawing.Font("Mongolian Baiti", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEquals.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonEquals.Location = new System.Drawing.Point(427, 20);
+            this.buttonEquals.Name = "buttonEquals";
+            this.buttonEquals.Size = new System.Drawing.Size(118, 109);
+            this.buttonEquals.TabIndex = 16;
+            this.buttonEquals.Text = "=";
+            this.buttonEquals.UseVisualStyleBackColor = false;
+            this.buttonEquals.Click += new System.EventHandler(this.buttonEquals_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(557, 717);
+            this.Controls.Add(this.buttonEquals);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.buttonDivided);
+            this.Controls.Add(this.buttonMulti);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonMulti;
+        private System.Windows.Forms.Button buttonDivided;
+        private System.Windows.Forms.TextBox answer;
+        private System.Windows.Forms.Button buttonEquals;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        string inputField = "0";
+        double firstNum;
+        double result =0;
+        char operation;
+        bool flagMinus = false;
+        bool afterMulti = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Answer_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            input.Text += "1";
+            inputField += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            input.Text += "2";
+            inputField += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            input.Text += "3";
+            inputField += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            input.Text += "4";
+            inputField += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            input.Text += "5";
+            inputField += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            input.Text += "6";
+            inputField += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            input.Text += "7";
+            inputField += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            input.Text += "8";
+            inputField += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            input.Text += "9";
+            inputField += "9";
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            input.Text += "0";
+            inputField += "0";
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            input.Text += '+';
+            firstNum = Convert.ToDouble(inputField);
+            inputField = "0";
+            result += firstNum;
+            operation = '+';
+            flagMinus = true;
+            Console.WriteLine($"Plus result = {result}");
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            input.Text += '-';
+            firstNum = Convert.ToDouble(inputField);
+            inputField = "-";
+
+            if (flagMinus == false) { 
+                result = firstNum;
+                flagMinus = true;
+            }
+            else
+                result+=firstNum;
+            Console.WriteLine($"Minus result = {result}");
+            operation = '-';
+        }
+
+        private void buttonMulti_Click(object sender, EventArgs e)
+        {
+            input.Text += '*';
+
+            /*
+            if (afterMulti == true)
+            {
+                operation = '*';
+                afterMulti = false;
+                return;
+            }
+            firstNum = Convert.ToDouble(inputField);
+            inputField = "0";
+            */
+            
+            if (afterMulti == false)
+            {
+                firstNum = Convert.ToDouble(inputField);
+                inputField = "0";
+            }
+            else {
+                operation = '*';
+                afterMulti = false;
+                return;
+            }
+
+            if (flagMinus == false) {
+                result = firstNum;
+                flagMinus = true;
+            }
+            else
+                result *= firstNum;
+
+            operation = '*';
+            Console.WriteLine($"Multi result = {result}");
+        }
+
+        private void buttonDivided_Click(object sender, EventArgs e)
+        {
+            input.Text += '/';
+
+            /*
+            if (afterMulti == true) {
+                operation = '/';
+                afterMulti = false;
+                return;
+            }
+            firstNum = Convert.ToDouble(inputField);
+            inputField = "0";
+            */
+            
+            if (afterMulti == false)
+            {
+                firstNum = Convert.ToDouble(inputField);
+                inputField = "0";
+            }
+            else
+            {
+                operation = '/';
+                afterMulti = false;
+                return;
+            }
+            if (flagMinus == false)
+            {
+                result = firstNum;
+                flagMinus = true;
+            }
+            else
+                result /= firstNum;
+            operation= '/';
+            Console.WriteLine($"Divided result = {result}");
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEquals_Click(object sender, EventArgs e)
+        {
+
+            
+            switch (operation) {
+                case '+':
+                    result+=Convert.ToDouble(inputField);
+                    afterMulti = true;
+                    break;
+                case '-':
+                    result+=Convert.ToDouble(inputField);
+                    afterMulti = true;
+                    break;
+                case '*':
+                    result*=Convert.ToDouble(inputField);
+                    afterMulti = true;
+                    Console.WriteLine($"Multi switch result = {result}");
+                    break;
+                case '/':
+                    result/=Convert.ToDouble(inputField);
+                    afterMulti = true;
+                    Console.WriteLine($"Divided switch result = {result}");
+                    break;
+            }
+
+            answer.Text = result.ToString();
+            Console.WriteLine($"Equals result = {result}");
+            inputField = "0";
+        }
+    }
+}
